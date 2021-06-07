ec771b85226157e073229f4d2e826fa91e2df679
AMSOUL Omar <o.amsoul@gmail.com>
2021-06-07 22:59:44 +0100

    The products page for clients is added (stil some little things)

---
+++ script.js
@@ -0,0 +1,14 @@
+function GoToInforamtique () {
+    var title = "Informatique et Multimedia";
+    document.getElementById("nProducts").innerHTML = title;
+}
+
+function GoToHabillement () {
+    var title = "Habillement et Bien Etre";
+    document.getElementById("nProducts").innerHTML = title;
+}
+
+function GoToMaison () {
+    var title = "Maison et Jardin";
+    document.getElementById("nProducts").innerHTML = title;
+}
